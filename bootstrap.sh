84b4d4a02bbf13cbbde3ee813063ac16303c55cf
joe miller <joeym@joeym.net>
2012-05-02 10:22:51 -0700

    refactored bootstrap script. still very ubuntu specific, btw

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 chef_solo='/opt/opscode/bin/chef-solo'
+debs_needed="curl git ruby rubygems make gcc"
 
-if ! which curl >/dev/null; then
-    apt-get -y install curl
+## --- main ---
+
+## -- install any distro pkgs we need
+debs_to_install=''
+
+for deb in $debs_needed; do
+    if ! dpkg --status $deb >/dev/null 2>&1; then
+        debs_to_install=$(echo $debs_to_install $deb)
+    fi
+done
+
+if [ ! -z "$debs_to_install" ]; then
+    apt-get -y install $debs_to_install
 fi
 
-if ! which git >/dev/null; then
-    apt-get -y install git
-fi
-
-if ! which ruby >/dev/null; then
-    apt-get -y install ruby1.9.1
-fi
-
+## -- install chef omnibus package if not installed
 if [ ! -x "$chef_solo" ]; then
     curl -L http://www.opscode.com/chef/install.sh | bash
 fi
@@ -22,4 +27,10 @@ fi
 # git clone ... ?
 # cd chef && librarian-chef update
 
+if ! which librarian-chef >/dev/null; then
+    gem install librarian --no-rdoc --no-ri
+fi
 
+cd chef && librarian-chef update
+
+# do chef stuff TODO
