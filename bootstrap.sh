c0bfd585e605178d786b8ac4d4045b061664e14c
joe miller <joeym@joeym.net>
2012-05-02 13:39:46 -0400

    added some informational messages to bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ debs_needed="curl git ruby rubygems make gcc"
 ## --- main ---
 
 ## -- install any distro pkgs we need
+echo "==> Checking for missing dependencies.."
 debs_to_install=''
 
 for deb in $debs_needed; do
@@ -15,11 +16,13 @@ for deb in $debs_needed; do
 done
 
 if [ ! -z "$debs_to_install" ]; then
+    echo "==> Installing missing dependencies: $debs_to_install"
     apt-get -y install $debs_to_install
 fi
 
 ## -- install chef omnibus package if not installed
 if [ ! -x "$chef_solo" ]; then
+    echo "==> Chef not installed, installing the omnibus package"
     curl -L http://www.opscode.com/chef/install.sh | bash
 fi
 
@@ -28,9 +31,9 @@ fi
 # cd chef && librarian-chef update
 
 if ! which librarian-chef >/dev/null; then
+    echo "==> Librarian not installed, installing.."
     gem install librarian --no-rdoc --no-ri
 fi
 
+echo "==> Running 'librarian-chef update'"
 cd chef && librarian-chef update
-
-# do chef stuff TODO
