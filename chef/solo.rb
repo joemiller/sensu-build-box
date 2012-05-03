7a05bdf61bf52ebfe5515fbbe8a78bf878f9018b
joe miller <joeym@joeym.net>
2012-05-03 09:20:54 -0400

    updated chef cache path

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,5 +1,8 @@
 root = File.expand_path(File.dirname(__FILE__))
- 
-file_cache_path root
+
+unless File.exists?('/var/chef/cache/tmp')
+  Dir.mkdir('/var/chef/cache/tmp')
+end 
+file_cache_path '/var/chef/cache/tmp'
 cookbook_path root + '/cookbooks'
 role_path root + '/roles'
